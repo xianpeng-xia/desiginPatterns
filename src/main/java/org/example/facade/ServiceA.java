62ff1f6b575d970605d7893d0df3891e4d178d39
夏先鹏 <951256916>
2019-12-21 23:42:09 +0800

    外观模式

---
+++ src/main/java/org/example/facade/ServiceA.java
@@ -0,0 +1,10 @@
+package org.example.facade;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019/12/21 11:32 下午
+ */
+public interface ServiceA {
+
+    void methodA();
+}
---
+++ src/main/java/org/example/facade/ServiceC.java
@@ -0,0 +1,10 @@
+package org.example.facade;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019/12/21 11:34 下午
+ */
+public interface ServiceC {
+
+    void methodC();
+}
