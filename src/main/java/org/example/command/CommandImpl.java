05f7ba118bc2650ab9e37756156f68393c2922af
夏先鹏 <951256916@qq.com>
2019-12-21 14:44:19 +0800

    命令模式

---
+++ src/main/java/org/example/command/CommandImpl.java
@@ -0,0 +1,18 @@
+package org.example.command;
+
+/**
+ * @author 夏先鹏
+ * @date 2019/12/21
+ * @time 2:39 下午
+ */
+public class CommandImpl extends Command {
+
+    public CommandImpl(Receiver receive) {
+        super(receive);
+    }
+
+    @Override
+    public void execute() {
+        receive.reveive();
+    }
+}
---
+++ src/main/java/org/example/command/Test.java
@@ -0,0 +1,19 @@
+package org.example.command;
+
+/**
+ * @author 夏先鹏
+ * @date 2019/12/21
+ * @time 2:42 下午
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Receiver receiver = new Receiver();
+        Command cmd = new CommandImpl(receiver);
+        Invoker invoker = new Invoker();
+        invoker.setCommand(cmd);
+        invoker.execute();
+
+    }
+}
