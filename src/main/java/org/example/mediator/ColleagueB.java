39a92c6c419821bb4007f88a20e8e889e5f0a219
夏先鹏 <951256916>
2019-12-22 22:37:34 +0800

    中介者模式

---
+++ src/main/java/org/example/mediator/ColleagueB.java
@@ -0,0 +1,13 @@
+package org.example.mediator;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019/12/22 10:32 下午
+ */
+public class ColleagueB implements Colleague {
+
+    @Override
+    public void action() {
+        System.out.println("前台注意了");
+    }
+}
---
+++ src/main/java/org/example/mediator/Mediator.java
@@ -0,0 +1,10 @@
+package org.example.mediator;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019/12/22 10:28 下午
+ */
+public abstract class Mediator {
+
+    public abstract void notice(String content);
+}
