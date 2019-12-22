ce29aa9f3b3a3c85f671eee3ec4bf1487ff9806d
夏先鹏 <951256916>
2019-12-22 21:26:25 +0800

    解释器模式

---
+++ src/main/java/org/example/interpreter/Test.java
@@ -0,0 +1,19 @@
+package org.example.interpreter;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019/12/22 9:20 下午
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Context context = new Context();
+        context.add(new SimpleExpression());
+        context.add(new AdvanceExpression());
+        context.add(new SimpleExpression());
+
+        for (Expression expression : context.getList()) {
+            expression.interpret(context);
+        }
+    }
+}
