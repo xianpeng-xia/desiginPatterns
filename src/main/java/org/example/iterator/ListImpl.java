9d4ecbeab9185c2d30757c21f4aad6d8fb70e4af
夏先鹏 <951256916>
2019-12-22 22:13:08 +0800

    迭代器模式

---
+++ src/main/java/org/example/iterator/ListImpl.java
@@ -0,0 +1,39 @@
+package org.example.iterator;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019/12/22 9:47 下午
+ */
+public class ListImpl implements List {
+
+    private Object[] list;
+    private int index;
+    private int size;
+
+    public ListImpl() {
+        this.list = new Object[100];
+        this.index = 0;
+        this.size = 0;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new IteratorImpl(this);
+    }
+
+    @Override
+    public Object get(int index) {
+        return list[index];
+    }
+
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
+    @Override
+    public void add(Object object) {
+        list[index++] = object;
+        size++;
+    }
+}
