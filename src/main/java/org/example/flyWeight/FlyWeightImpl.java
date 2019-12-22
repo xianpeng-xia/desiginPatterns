6974ad1f038c52fceb270e1ac9da12eaa34fb18d
夏先鹏 <951256916>
2019-12-22 20:58:59 +0800

    享元模式

---
+++ src/main/java/org/example/flyWeight/FlyWeightImpl.java
@@ -0,0 +1,14 @@
+package org.example.flyWeight;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019/12/22 8:40 下午
+ */
+public class FlyWeightImpl implements FlyWeight {
+
+
+    @Override
+    public void action(int arg) {
+        System.out.println("参数值：" + arg);
+    }
+}
