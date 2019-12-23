82834c3de94edf79fa600a08916549223afae613
夏先鹏 <951256916@qq.com>
2019-12-23 11:23:18 +0800

    原型模式

---
+++ src/main/java/org/example/prototype/Test.java
@@ -0,0 +1,17 @@
+package org.example.prototype;
+
+/**
+ * @author 夏先鹏
+ * @date 2019/12/23
+ * @time 11:20
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Prototype prototype = new ConcreatePrototype("prototype");
+        Prototype clonePrototype = (Prototype) prototype.clone();
+        System.out.println(prototype.getName());
+        System.out.println(clonePrototype.getName());
+
+    }
+}
