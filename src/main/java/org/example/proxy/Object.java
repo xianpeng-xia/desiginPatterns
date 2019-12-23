963058350489374b01bcc60982c16689dbe42b9d
夏先鹏 <951256916@qq.com>
2019-12-23 11:46:04 +0800

    代理模式

---
+++ src/main/java/org/example/proxy/Object.java
@@ -0,0 +1,11 @@
+package org.example.proxy;
+
+/**
+ * @author 夏先鹏
+ * @date 2019/12/23
+ * @time 11:35
+ */
+public interface Object {
+
+    void action();
+}
---
+++ src/main/java/org/example/proxy/Test.java
@@ -0,0 +1,14 @@
+package org.example.proxy;
+
+/**
+ * @author 夏先鹏
+ * @date 2019/12/23
+ * @time 11:42
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        ProxyObject proxyObject = new ProxyObject();
+        proxyObject.action();
+    }
+}
