3a8c004ce5e4441bdcdbe51327cf44304ae51daf
夏先鹏 <951256916>
2020-05-20 23:37:27 +0800

    防止反射和序列化破坏 静态内部类单例

--- src/main/java/org/example/singleton/LazyInnerClassSingleton.java
+++ src/main/java/org/example/singleton/LazyInnerClassSingleton.java
@@ -1,5 +1,7 @@
 package org.example.singleton;
 
+import java.io.Serializable;
+
 /**
  * Created by xianpeng.xia
  * on 2020/5/20 11:24 下午
@@ -7,14 +9,21 @@ package org.example.singleton;
 public class LazyInnerClassSingleton {
 
     private LazyInnerClassSingleton() {
+        if (LazyHolder.INSTANCE != null) {
+            throw new RuntimeException("反射生成单例失败");
+        }
     }
 
     public static final LazyInnerClassSingleton getInstance() {
         return LazyHolder.INSTANCE;
     }
 
-    private static class LazyHolder {
+    private static class LazyHolder implements Serializable {
 
         private static final LazyInnerClassSingleton INSTANCE = new LazyInnerClassSingleton();
+
+        private Object readResolve() {
+            return LazyHolder.INSTANCE;
+        }
     }
 }
